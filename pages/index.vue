81a2db5875d557602b6dd3e0c4ecd47faebb8fc0
notfingees <75046362+notfingees@users.noreply.github.com>
2022-05-08 16:50:42 -0500

    client-only additions

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,14 @@
 <body>
 <div id="app">
   <div id="container">
+
+    <client-only>
     <div v-on:click="switch_donny" class="left">
      <!-- <h2 id="shuffle">CLICK TO SHUFFLE</h2> -->
             <pre id="landing_donny">{{five_hundred_nfts[current_landing_donny_index]}}</pre>
         <button @click="mint(five_hundred_nfts[current_landing_donny_index])" id="mint_button" class="hide-mobile">MINT NOW</button>
       </div>
+      </client-only>
 
     <div class="right">
 
@@ -42,7 +45,9 @@
         <!--<div v-for="nft in fivebyfive_animated" :key="nft">
           <pre class="fivebyfive_nft">{{nft}}</pre>
           </div>-->
+          <client-only>
           <pre class="fivebyfive_nft">{{fivebyfive_animated[currently_shown_fivebyfive_animated_index]}}</pre>
+          </client-only>
           
           </div>
         </div>
@@ -98,7 +103,7 @@ c  >|
     <section id="mint">
       
       <div class="mint_div">
-        <!--<NFTCreator/>-->
+        <NFTCreator/>
 
         </div>
 
